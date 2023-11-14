8205a8538d02a845f60ca1f20c2ebf8cca657036
Vic Khov <victorkhovtko@gmail.com>
2023-11-14 04:09:44 +0300

    added utils and StDiff local FastAPI demo

---
+++ api/utils.py
@@ -0,0 +1,11 @@
+import hashlib
+import datetime
+
+
+def get_image_name(key: str, file_extension: str) -> str:
+    # Get the current timestamp in a human-readable format
+    timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+
+    # Combine the timestamp with the key and generate a unique hash
+    unique_hash = hashlib.sha256(f"{timestamp}_{key}".encode()).hexdigest()
+    return f"{timestamp}_{unique_hash}.{file_extension}"
